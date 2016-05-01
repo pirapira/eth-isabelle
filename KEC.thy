518b04c31ed98d41e13810b7da09135ec42321d9
Yoichi Hirai <i@yoichihirai.com>
2016-05-01 23:19:16 +0200

    sha3 "" matches.

--- KEC.thy
+++ KEC.thy
@@ -63,8 +63,6 @@ definition "(theta1 (i :: nat) (st :: 64 word list) :: 64 word) =
 definition "(theta_t (i :: nat) (bc :: 64 word list) :: 64word) =
   (bc ! ((i + 4) mod 5)) XOR (rotl64 (bc ! ((i + 1) mod 5)) 1)
   "
-
-value "upt 0 5"
   
 definition "((theta (st :: 64 word list)) :: 64 word list) =
   (let (bc :: 64 word list) = map (\<lambda> i. theta1 i st) (upt 0 5) in
@@ -72,17 +70,12 @@ definition "((theta (st :: 64 word list)) :: 64 word list) =
   map (\<lambda> ji. (st ! ji) XOR (t ! (ji mod 5))) (upt 0 25)
   ))
   "
-  
-definition "st_test = [(1 :: 64 word), 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
-              14,
-              15, 16, 17, 18, 19,
-              20, 21, 22, 23, 24, 25]"
-              
-value "map (\<lambda> i. theta1 i st_test) (upt 0 5)"
-value "theta st_test"
+
+definition "(st_test :: 64 word list) = 
+ [6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9223372036854775808, 
+0, 0, 0, 0, 0, 0, 0, 0]"
 
 text "For Rho Pi transformation, I need an update function for the list."
-value "[1,2,3][1 := 0]"
 
 text "For a fixed i, Rho Pi maps (t, st) to (t, st)."
 definition "(rho_pi_inner (i :: nat) (t_st :: 64 word \<times> 64 word list)
@@ -91,33 +84,34 @@ definition "(rho_pi_inner (i :: nat) (t_st :: 64 word \<times> 64 word list)
   let bc = (snd t_st) ! j in
  (bc, (snd t_st)[j := rotl64 (fst t_st) (keccakf_rotc ! i)]))
  "
- 
- value "rho_pi_inner 3 (0, st_test)"
- 
-definition "(rho_pi_changes (t_st :: 64 word \<times> 64 word list)
+
+definition "(rho_pi_changes (i :: nat) (t_st :: 64 word \<times> 64 word list)
  :: (64 word \<times> 64 word list)) =
- foldr rho_pi_inner (upt 0 24) t_st
+ fold rho_pi_inner (upt 0 i) t_st
  "
- 
+
 definition "(rho_pi (st :: 64 word list) :: 64 word list) =
-snd (rho_pi_changes (st ! 1, st))"
- 
+snd (rho_pi_changes 24 (st ! 1, st))"
+
 text "model chi"
 text "model it as a map on st?  Or, a five-time iteration?"
 text "Maybe, [0..4] [5..9] [10..14] and so on"
 
 text "First an operation that works on slices of length five."
 definition "(chi_for_j (st_slice :: 64 word list) :: 64 word list) =
-  map (\<lambda> i. st_slice ! i XOR (NOT st_slice ! ((i + 1) mod 5)) XOR
-            st_slice ! ((i + 2) mod 5)) (upt 0 5)
+  map (\<lambda> i. (st_slice ! i) XOR ((NOT (st_slice ! ((i + 1) mod 5)))
+            AND
+            (st_slice ! ((i + 2) mod 5))))
+            (upt 0 5)
 "
 
-value "sublist st_test (set (upt 0 5))"
+definition "slice0 = (sublist (rho_pi (theta st_test)) (set (upt 0 5)))"
 
 definition "(chi (st :: 64 word list) :: 64 word list) =
-  concat (map (\<lambda> j. chi_for_j (sublist st (set (upt (j * 5) (j * 5 + 5))))) (upt 0 5))"
-
-value "chi st_test"
+  concat (map (\<lambda> j. chi_for_j
+                    (sublist st (set (upt (j * 5) (j * 5 + 5))))
+                    )
+                    (upt 0 5))"
 
 text "model iota"
 
@@ -132,8 +126,21 @@ definition "(for_inner (r :: nat) (st :: 64 word list) :: 64 word list) =
 
 definition "(keccakf_rounds :: nat) = 24"
 
+type_synonym "byte" = "8 word"
+
+definition "split" :: "64 word \<Rightarrow> byte list"
+where
+"split = word_rsplit"
+
+definition "cat" :: "byte list \<Rightarrow> 64 word"
+where "cat = word_rcat"
+
+definition "invert_endian" :: "64 word \<Rightarrow> 64 word"
+where
+"invert_endian w = cat (rev (split w))"
+
 definition "(keccakf (st :: 64 word list) :: 64 word list) =
-  foldr for_inner (upt 0 keccakf_rounds) st"
+   (fold for_inner (upt 0 keccakf_rounds) st)"
 
 value "keccakf st_test"
 
@@ -144,22 +151,17 @@ definition "(mdlen :: nat) = 256 div 8"
 definition "(rsiz :: nat) = 200 - mdlen * 2"
 
 
-type_synonym "byte" = "8 word"
 
 definition "(update_byte (i :: byte) (p :: nat) (st :: 64 word list)
 :: 64 word list) =
-st[p div 8 := (st ! (p div 8)) XOR ((ucast i) << (56 - 8 * (p mod 8))) ]
+st[p div 8 := (st ! (p div 8)) XOR ((ucast i) << (8 * (p mod 8))) ]
 "
-
-value "(3 :: nat) div 8"
-value "update_byte 2 3 [0, 0, 0]" (* just remember big endian *)
-value "(ucast (2 :: 8 word) :: 64 word) << (56 - 8 * (3 mod 8))"
 
 text "model sha3_update"
 text "recursion on the length of the input."
 text "But which command shall I use?  primrec?"
 
-primrec "sha3_update" :: 
+primrec "sha3_update" ::
 "byte list (* input *) \<Rightarrow> nat (* p *)
 \<Rightarrow> 64 word list (* st *) \<Rightarrow> (nat (* final p *) \<times> 64 word list)"
 where
@@ -167,7 +169,7 @@ where
 "sha3_update (c # rest) pos st =
   (if (pos \<le> rsiz)
    then
-     sha3_update rest (pos + 1) (update_byte c pos st) 
+     sha3_update rest (pos + 1) (update_byte c pos st)
    else
      sha3_update rest 0 (keccakf (update_byte c pos st))
   )
@@ -177,10 +179,6 @@ value "sha3_update [1, 2, 3] 0 st_test"
 
 text "model sha3_final"
 
-definition "split" :: "64 word \<Rightarrow> byte list"
-where
-"split = word_rsplit"
-
 value "split 30"
 
 definition "sha3_final" :: "nat \<Rightarrow> 64 word list \<Rightarrow> byte list"
@@ -189,7 +187,7 @@ where
    (let st0 = update_byte 0x06 p st in
     let st1 = update_byte 0x80 (rsiz - 1) st0 in
     let st2 = take 4 (keccakf st1) in
-    concat (map split st2))
+    concat (map (\<lambda> x. rev (split x)) st2))
 "
 
 text "model the whole sha3 function"
@@ -206,7 +204,7 @@ where
 "sha3 input =
    (let mid = sha3_update input initial_pos initial_st in
     sha3_final (fst mid) (snd mid))"
-    
-value "sha3 [0, 0, 0]"
+
+value "sha3 []"
 
 end
