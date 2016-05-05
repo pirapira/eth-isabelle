75f90075107e538c66e7ce36bc83c1f1758ad17a
Yoichi Hirai <i@yoichihirai.com>
2016-05-05 20:50:31 +0200

    Trying some proofs on RLP.

--- RLP.thy
+++ RLP.thy
@@ -22,6 +22,26 @@ where
 
 value "BE 300"
 
+fun BD_rev :: "byte list \<Rightarrow> nat"
+where
+"BD_rev [] = 0" |
+"BD_rev (h # t) = 256 * (BD_rev t) + (unat h)"
+
+lemma BD_rev_BE_rev [simp]: "BD_rev (BE_rev n) = n"
+apply(induct n rule: BE_rev.induct)
+apply(simp)
+apply(auto)
+  apply(simp add: Word.unat_word_ariths(1) Word.unat_of_nat)
+apply(simp add: Word.unat_of_nat)
+done
+
+definition BD :: "byte list \<Rightarrow> nat"
+where
+"BD lst = BD_rev (rev lst)"
+
+lemma BD_BE [simp]: "BD (BE n) = n"
+by (simp add: BD_def BE_def)
+
 fun "r_b" :: "byte list \<Rightarrow> byte list"
 where
 "r_b [] = [128]" |
@@ -35,6 +55,17 @@ where
 
 value "r_b (map of_nat (upt 0 100))"
 
+function "de_r_b" :: "byte list \<Rightarrow> (byte list * (*rest*) byte list) option"
+where
+  "de_r_b [] = None"
+| "de_r_b (128 # r) = Some ([], r)"
+| "k < 128 \<Longrightarrow> de_r_b (k # r) = Some ([k], r)"
+| "128 < k \<Longrightarrow> k < 128 + 56 \<Longrightarrow>
+    de_r_b (k # lst) = (if length lst \<ge> (unat k) then Some (take (unat k) lst, drop (unat k) lst)
+                                         else None)"
+apply(auto)
+oops
+
 fun "RLP" :: "tree \<Rightarrow> byte list"
 where
 "RLP(Leaf l) = r_b l" |
@@ -46,6 +77,10 @@ where
     else
       of_nat (247 + length (BE len_s)) # (BE len_s @ s) ))"
 
+definition "RLP_nat" :: "nat \<Rightarrow> byte list"
+where
+"RLP_nat i = RLP (Leaf (BE i))"
+      
 text "TODO :: also define a decoder and say something"
 
 end
