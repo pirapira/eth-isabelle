ca19684ca969127e02024df595cb9740eb48baa1
Yoichi Hirai <i@yoichihirai.com>
2016-10-07 23:53:08 +0200

    Defined bool_to_uint

---
+++ ContractSem.thy
@@ -0,0 +1,12 @@
+text "This is a port of ContractSem.v in pirapira/evmverif"
+
+text "The main difficulty is to avoid the use of coinduction"
+
+text "One way is, in the specification, talk about some concrete state."
+text "Maybe that's fine."
+
+import Main "./Instructions"
+
+start
+
+end
