d3ee8e398b657da17876ee9bd2f2020a08f77ff3
Yoichi Hirai <i@yoichihirai.com>
2016-10-07 23:58:51 +0200

    Define call_arguments

--- ContractSem.thy
+++ ContractSem.thy
@@ -5,8 +5,29 @@ text "The main difficulty is to avoid the use of coinduction"
 text "One way is, in the specification, talk about some concrete state."
 text "Maybe that's fine."
 
-import Main "./Instructions"
+theory ContractSem
 
-start
+imports Main "~~/src/HOL/Word/Word" "./Instructions"
+
+begin
+
+type_synonym uint = "256 word"
+type_synonym address = "160 word"
+type_synonym byte = "8 word"
+
+definition bool_to_uint :: "bool \<Rightarrow> uint"
+where
+"bool_to_uint b = (if b then 1 else 0)"
+
+definition "drop_one_element = tl"
+
+record call_arguments =
+  callarg_gaslimit :: uint
+  callarg_code :: address
+  callarg_recipient :: address
+  callarg_value :: uint
+  callarg_data :: "byte list"
+  callarg_output_begin :: uint
+  callarg_output_size :: uint
 
 end
