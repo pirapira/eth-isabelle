a20f55c0ea7353b517102521256fd2a96eec5e5c
Yoichi Hirai <i@yoichihirai.com>
2016-10-13 18:27:13 +0200

    Defined transitive closure of operations and induction lemmata

--- RelationalSem.thy
+++ RelationalSem.thy
@@ -1,5 +1,8 @@
 theory RelationalSem
 
+(* This is a relational semantics trying to capture
+ * one element of the call stack *)
+
 imports Main "./ContractSem"
 
 begin
@@ -17,19 +20,19 @@ natural:
 inductive world_turn :: "(account_state * instruction_result) \<Rightarrow> (account_state * variable_env) \<Rightarrow> bool"
 where
   world_continue: "world_turn (orig, (InstructionContinue (v, _))) (orig, v)"
-| world_call:
+(* | world_call: commented out because we are not going into the deeper callee
   "account_state_natural_change old_state new_state \<Longrightarrow>
    build_venv_called old_state callargs next_venv \<Longrightarrow>
    \<not> (instruction_result_continuing old_result) \<Longrightarrow>
-   world_turn (old_state, old_result) (next_state, next_venv)"
+   world_turn (old_state, old_result) (next_state, next_venv)" *)
 | world_return:
   "account_state_natural_change account_state_going_out account_state_back \<Longrightarrow>
    build_venv_returned account_state_back result new_v \<Longrightarrow>
-   world_turn (account_state_going_out, (InstructionToWorld (_, _, _, Some _))) (account_state_back, new_v)"
+   world_turn (account_state_going_out, (InstructionToWorld (_, _, _, _))) (account_state_back, new_v)"
 | world_fail:
   "account_state_natural_change account_state_going_out account_state_back \<Longrightarrow>
    build_venv_failed account_state_back = Some new_v \<Longrightarrow>
-   world_turn (account_state_going_out, (InstructionToWorld (_, _, _, Some _))) (account_state_back, new_v)"
+   world_turn (account_state_going_out, (InstructionToWorld (_, _, _, _))) (account_state_back, new_v)"
 
 abbreviation next_instruction :: "variable_env \<Rightarrow> inst \<Rightarrow> bool"
 where
@@ -72,14 +75,37 @@ where
 inductive star :: "('a \<Rightarrow> 'a \<Rightarrow> bool) \<Rightarrow> 'a \<Rightarrow> 'a \<Rightarrow> bool"
 where
 refl: "star r x x" |
-step: "star r x y \<Longrightarrow> r y z \<Longrightarrow> star r x z"
+step: "r x y \<Longrightarrow> star r y z \<Longrightarrow> star r x z"
 
 inductive reachable :: "account_state \<Rightarrow> (account_state * instruction_result) \<Rightarrow> bool"
 where
 "initial_instruction_result original init \<Longrightarrow>
  star one_step (original, init) fin \<Longrightarrow>
  reachable original fin"
- 
+
+lemma star_ind :
+"
+star r init dst \<Longrightarrow>
+P init \<Longrightarrow>
+(\<forall> next future. r init next \<longrightarrow>
+ star r next future \<longrightarrow> P future)
+\<Longrightarrow> P dst
+"
+apply(induction rule: star.induct; auto)
+done
+
+lemma reachable_ind :
+"(\<forall> init. initial_instruction_result a init \<longrightarrow>
+   (P (a, init) \<and>
+     (\<forall> next future. one_step (a, init) next \<longrightarrow>
+                     star one_step next future \<longrightarrow> P future )
+   )) \<Longrightarrow>
+ reachable a f \<Longrightarrow> P f"
+apply(erule reachable.cases)
+apply(auto)
+apply(erule star_ind; auto)
+done
+
 inductive one_run :: "account_state \<Rightarrow> (account_state * instruction_result) \<Rightarrow> bool"
 where
 "initial_instruction_result original init \<Longrightarrow>
@@ -91,13 +117,18 @@ where
 "no_assertion_failure a ==
   \<forall> fin r. reachable a (fin, r) \<longrightarrow>
   r \<noteq> InstructionAnnotationFailure"
-  
+
+(* TODO: define calls_of_code *)
+
+(*
 definition no_assertion_failure_one_run :: "program \<Rightarrow> bool"
 where
 "no_assertion_failure_one_run code ==
  \<forall> a fin r.
  account_code a = code \<longrightarrow>
+ calls_of_code code (account_ongoing_calls a) \<longrightarrow>
  one_run a (fin, r) \<longrightarrow>
  r \<noteq> InstructionAnnotationFailure"
 
+*)
 end
