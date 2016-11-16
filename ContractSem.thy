a9c54557b0aef6c8f5e5cf9d9942625ce517b744
Yoichi Hirai <i@yoichihirai.com>
2016-11-16 19:03:54 +0100

    Snapshot

--- ContractSem.thy
+++ ContractSem.thy
@@ -14,14 +14,10 @@
    limitations under the License.
 *)
 
-section "A Contract Centric View of the EVM"
-
-text {* Here is a presentation of the Ethereum Virtual Machine (EVM) in a form
-suitable for formal verification of a single account.  *}
-
 theory ContractSem
 
 imports Main "~~/src/HOL/Word/Word" "./ContractEnv" "./Instructions" "./KEC" "./lem/Evm"
+(* If ./lem/Evm is missing, try executing `make lem-thy` *)
 
 begin
 
@@ -29,18 +25,9 @@ declare venv_advance_pc_def [simp]
 declare venv_next_instruction_def [simp]
 declare call_def [simp]
 
-subsection "Utility Functions"
-
-text {* The following function is an if-sentence, but with some strict control
-over the evaluation order.  Neither the then-clause nor the else-clause
-is simplified during proofs.  This prevents the automatic simplifier from
-computing the results of both the then-clause and the else-clause.
-*}
-
 text {* When the if-condition is known to be True, the simplifier can
 proceed into the then-clause.  The \textit{simp} attribute encourages the simplifier
 to use this equation from left to right whenever applicable.  *}
-
 lemma strict_if_True [simp] :
 "strict_if True a b = a True"
 apply(simp add: strict_if_def)
@@ -48,7 +35,6 @@ done
 
 text {* When the if-condition is known to be False, the simplifier
 can proceed into the else-clause. *}
-
 lemma strict_if_False [simp] :
 "strict_if False a b = b True"
 apply(simp add: strict_if_def)
@@ -59,167 +45,38 @@ the simplifier is allowed to perform computation on the if-condition.
 The \textit{cong} attribute tells the simplifier to try to rewrite the
 left hand side of the conclusion, using the assumption.
 *}
-
 lemma strict_if_cong [cong] :
 "b0 = b1 \<Longrightarrow> strict_if b0 x y = strict_if b1 x y"
 apply(auto)
 done
 
-subsection "The Interaction between the Contract and the World"
-
-text {*In this development, the EVM execution is seen as an interaction between a single contract
-invocation
-and the rest of the world.  The world can call into the contract.  The contract can reply by just
-finishing or failing, but it can also call an account\footnote{This might be the same account as our
-invocation, but still the deeper calls is part of the world.}.  When our contract execution calls an account,
-this is seen as an action towards the world, because the world then has to decide the
-result of this call.  The world can say that the call finished successfully or exceptionally.
-The world can also say that the call resulted in a reentrancy.  In other words,
-the world can call the contract again and change the storage and the balance of our contract.
-The whole process is captured as a game between
-the world and the contract. *}
-
-subsubsection "The Contract's Moves"
-
-text {* After being invoked, the contract can respond by calling an account, creating (or deploying)
-a smart contract, destroying itself, returning, or failing.  When the contract calls an account,
-the contract provides the following information.*}
-
-text {* When our contract deploys a smart contract, our contract should provide the following
-information. *}
-
-text {* The contract's moves are summarized as follows. *}
-
-subsection "Program Representation"
-
-text "For performance reasons, the instructions are stored in an AVL tree that allows
-looking up instructions from the program counters."
-
-text {* The empty program is easy to define. *}
-
 declare empty_program_def [simp]
-
-subsection "Translating an Instruction List into a Program"
-
-subsubsection {* Storing annotations in a program in a mapping *}
-
-text {* Annotations are stored in a mapping that maps positions into lists of annotations.
-The rationale for this data structure is that a single position might contain multiple annotations.
-Here is a function that inserts an annotation
-at a specified position. *}
-
 declare prepend_annotation_def [simp]
-
 declare program_annotation_of_lst.simps [simp]
-
-subsubsection {* Translating a list of instructions into a program *}
-
-text {* The results of the above translations are packed together in a record. *}
-text {* For efficiency reasons, the program content is going to be packed as
-an AVL tree, but this particular encoding is not part of the Lem definition.
-So such encoders are parametrised here.*}
-
 declare program_of_lst_def [simp]
-
-subsection {* Program as a Byte Sequence *}
-
-text {* For CODECOPY instruction, the program must be seen as a byte-indexed read-only memory. *}
-text {* Such a memory is here implemented by a lookup on an AVL tree.*}
-
 declare program_as_memory_def [simp]
    
-subsection {* Execution Environments *}
-
-text "I model an instruction as a function that takes environments and modifies some parts of them."
-
-text "The execution of an EVM program happens in a block, and the following information about
-the block should be available."
-
-text {* The variable environment contains information that is relatively volatile. *}
-
-text {* The constant environment contains information that is rather stable. *}
-
 subsection {* The Result of an Instruction *}
 
-text {* The result of program execution is microscopically defined by results of instruction
-executions.  The execution of a single instruction can result in the following cases: *}
-
-text {* When the contract fails, the result of the instruction always looks like this: *}
-    
 declare instruction_failure_result_def [simp]
 
 text {* When the contract returns, the result of the instruction always looks like this: *}
   
 declare instruction_return_result_def [simp]
-
-subsection {* Useful Functions for Defining EVM Operations *}
-
-text {* Currently the GAS instruction is modelled to return random numbers.
-The random number is not known to be of any value.
-However, the value is not unknown enough in this formalization because
-the value is only dependent on the variable environment (which does not
-keep track of the remaining gas).  This is not a problem as long as
-we are analyzing a single invocation of a loopless contract, but
-gas accounting is a planned feature.
-*}
-
-text {* This $M$ function is defined at the end of H.1.\,in the yellow paper.
-This function is useful for updating the memory usage counter. *}
-
 declare M_def [simp]
-
-text {* Updating a balance of a single account:  *}
 declare update_balance_def [simp]
-
-text {* Popping stack elements: *}
 declare venv_pop_stack.simps [simp]
-
-text {* Peeking the topmost element of the stack: *}
 declare venv_stack_top_def [simp]
-
-text {* Updating the storage at an index: *}
-
 declare venv_update_storage_def [simp]
-  
-text {* No-op, which just advances the program counter: *}
 declare stack_0_0_op_def [simp]
-
-text {* A general pattern of operations that pushes one element onto the stack:  *}
 declare stack_0_1_op_def [simp]
-
-text {* A general pattern of operations that transforms the topmost element of the stack: *}
 declare stack_1_1_op_def [simp]
-
-text {* A general pattern of operations that consume one word and produce two rwords: *}
 declare stack_1_2_op_def [simp]
-
-text {* A general pattern of operations that take two words and produce one word: *}
 declare stack_2_1_op_def [simp]
-
-text {* A general pattern of operations that take three words and produce one word: *}
 declare stack_3_1_op_def [simp]
-
-subsection {* Definition of EVM Operations *}
-
-text "SSTORE changes the storage so it does not fit into any of the patterns defined above."
 declare sstore_def [simp]
-
-text "For interpreting the annotations, I first need to construct the annotation environment
-out of the current execution environments.  When I try to remove this step, I face some
-circular definitions of data types."
 declare build_aenv_def [simp]
-
-text "In reality, EVM programs do not contain annotations so annotations never cause failures.
-However, during the verification, I want to catch annotation failures.  When the annotation
-evaluates to False, the execution stops and results in @{term InstructionAnnotationFailure}."
-    
-text "The JUMP instruction has the following meaning.  When it cannot find the JUMPDEST instruction
-at the destination, the execution fails."
 declare jump_def [simp]
-
-text {* This function is a reminiscent of my struggle with the Isabelle/HOL simplifier.
-The second argument has no meaning but to control the Isabelle/HOL simplifier.
-*}
 
 text {* When the second argument is already @{term True}, the simplification can continue.
 Otherwise, the Isabelle/HOL simplifier is not allowed to expand the definition of
@@ -238,96 +95,32 @@ lemma unblock_jump [simp]:
 apply(simp add: blocked_jump_def)
 done
 
-text {* The JUMPI instruction is implemented using the JUMP instruction. *}
-
 declare jumpi_def [simp]
-
-text {* Looking up the call data size takes this work: *}
 declare datasize_def [simp]
-
-text {* Looking up a word from a list of bytes: *}
 declare read_word_from_bytes_def [simp]
-
-text {* Looking up a word from the call data: *}
 declare cut_data_def [simp]
-
-text {* Looking up a number of bytes from the memory: *}
 declare cut_memory.simps [simp]
-
-text {* CALL instruction results in @{term ContractCall} action when there are enough stack elements
-        (and gas, when we introduce the gas accounting). *}
-
-text {* DELEGATECALL is slightly different. *}
-
 declare delegatecall_def [simp]
-
-text {* CALLCODE is another variant. *}
-
 declare callcode_def [simp]
-
-text "CREATE is also similar because the instruction causes execution on another account."
-
 declare create_def [simp]
-
-text "RETURN is modeled like this:"
 declare ret_def [simp]
-
-text "STOP is simpler than RETURN:"
 declare stop_def [simp]
-
-text "POP removes the topmost element of the stack:"
 declare pop_def [simp]
-
-text "A utility function for storing a list of bytes in the memory:"
-
 declare store_byte_list_memory.psimps [simp]
-
-text "Using the function above, it is straightforward to store a byte in the memory."
 declare store_word_memory_def [simp]
-
-text "MSTORE writes one word to the memory:"
-
 declare mstore_def [simp]
-
-text "MLOAD reads one word from the memory:"
-
 declare mload_def [simp]
-
-text "MSTORE8 writes one byte to the memory:"
-
 declare mstore8_def [simp]
-
-text "For CALLDATACOPY, I need to look at the caller's data as memory."
-
 declare input_as_memory_def [simp]
-
-text "CALLDATACOPY:"
 declare calldatacopy_def [simp]
-
-text "CODECOPY copies a region of the currently running code to the memory:"
 declare codecopy_def [simp]
-
-text "EXTCODECOPY copies a region of the code of an arbitrary account.:"
 declare extcodecopy_def [simp]
-
-text "PC instruction could be implemented by @{term stack_0_1_op}:"
 declare pc_def [simp]
-
-text "Logging is currently no-op, until some property about event logging is wanted."
-     
 declare log_def [simp]
 
-text "For SWAP operations, I first define a swap operations on lists."
-definition list_swap :: "nat \<Rightarrow> 'a list \<Rightarrow> 'a list option"
-where
-"list_swap n lst =
-  (if length lst < n + 1 then None else
-  Some (List.concat [[lst ! n], take (n - 1) (drop 1 lst) , [lst ! 0], drop (1 + n) lst]))"
-  
 declare list_swap_def [simp]
 
 text "For testing, I prove some lemmata:"
-
 lemma "list_swap 1 [0, 1] = Some [1, 0]"
 apply(auto)
 done
@@ -348,13 +141,8 @@ lemma"list_swap 1 [0, 1, 2, 3] = Some [1, 0, 2, 3]"
 apply(auto)
 done
 
-text "Using this, I can specify the SWAP operations:"
-
 declare swap_def [simp]
 
-text {* SHA3 instruciton in the EVM is actually reaak 256.
-In this development, Keccak256 computation is defined in KEC.thy.
-*}
 definition sha3 :: "variable_env \<Rightarrow> constant_env \<Rightarrow> instruction_result"
 where
 "sha3 v c \<equiv>
