4dc2d90cd49883295ffc1b31b2232cfee937f6ea
Yoichi Hirai <i@yoichihirai.com>
2016-12-14 20:11:23 +0100

    Parse test cases

--- tester/jsonTest.ml
+++ tester/jsonTest.ml
@@ -1,8 +1,36 @@
+open Yojson.Basic
+
+type test_case =
+  { callcreates : json
+  ; env : json
+  ; exec : json
+  ; gas : json
+  ; logs : json
+  ; out : json
+  ; post : json
+  ; pre : json
+  }
+
+let parse_test_case (j : json) : test_case =
+  Util.(
+  { callcreates = member "callcreates" j
+  ; env = member "env" j
+  ; exec = member "exec" j
+  ; gas = member "gas" j
+  ; logs = member "logs" j
+  ; out = member "out" j
+  ; post = member "post" j
+  ; pre = member "pre" j
+  })
+
 let () =
-  let open Yojson.Basic in
-  let vmArithmeticTest : json = Yojson.Basic.from_file "../tests/VMTests/vmArithmeticTest.json" in
-  let vmArithmeticTestAssoc : (string * json) list = Util.to_assoc vmArithmeticTest in
+  let vm_arithmetic_test : json = Yojson.Basic.from_file "../tests/VMTests/vmArithmeticTest.json" in
+  let vm_arithmetic_test_assoc : (string * json) list = Util.to_assoc vm_arithmetic_test in
   let () =
-    List.iter (fun (label, elm) -> Printf.printf "%s\n" label) vmArithmeticTestAssoc
+    List.iter (fun (label, elm) ->
+        let () = Printf.printf "%s\n" label in
+        let case : test_case = parse_test_case elm in
+        ()
+      ) vm_arithmetic_test_assoc
   in
   ()
