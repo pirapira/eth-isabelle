6798832b1af5220b7fc4c4f769f8f507ab9c09cc
Yoichi Hirai <i@yoichihirai.com>
2016-12-16 19:02:16 +0100

    Big_int to/from Word256 conversion

--- tester/evmTest.ml
+++ tester/evmTest.ml
@@ -1,11 +1,32 @@
 open Evm
 
-let word256_of_big_int (b : Big_int.big_int) =
-  let binary = BatBig_int.to_string_in_binary b in
-  (** now here, the first char might be - or +, you can say, not supporting negative *)
-  binary
+let pad_left (elm : 'a) (len : int) (orig : 'a list) =
+  let remaining = len - List.length orig in
+  let () = Printf.printf "padding: remaining: %d\n%!" remaining in
+  let padding = BatList.make remaining elm in
+  padding @ orig
 
+let word256_of_big_int (b : Big_int.big_int) =
+  let () = if BatBig_int.(lt_big_int b zero_big_int) then failwith "negative number cannot be turned into word256" else () in
+  let binary : string = BatBig_int.to_string_in_binary b in
+  (* should be a sequence of 0s and 1s *)
+  let bl : bool list =
+    List.map (fun (digit : char) ->
+        match digit with
+        | '0' -> false
+        | '1' -> true
+        | _ -> failwith "neither 0 or 1"
+      ) (BatString.to_list binary)
+    in
+  let (h :: tl) = pad_left false 256 bl in
+  Word256.W256 (h, tl)
+
+let big_int_of_word256 (Word256.W256 (h, tl)) : Big_int.big_int =
+  let bl = h :: tl in
+  let nums : Big_int.big_int list = List.map (fun x -> if x then Big_int.unit_big_int else Big_int.zero_big_int) bl in
+  List.fold_left (fun x y -> Big_int.(add_big_int y (mult_int_big_int 2 x))) Big_int.zero_big_int nums
 
 let () =
   let x : Word256.word256 = Word256.W256 (true, []) in
-  Printf.printf "hello %s\n" (word256_of_big_int (BatBig_int.big_int_of_int (-333)))
+  let open Big_int in
+  Printf.printf "hello %s\n" (string_of_big_int (big_int_of_word256 (word256_of_big_int (BatBig_int.big_int_of_int (333)))))
