991027951263565bb6bb4b8acdf103cf928bc7ff
Yoichi Hirai <i@yoichihirai.com>
2016-12-19 15:27:19 +0100

    Somehow printing the EVM program result

--- tester/evmTest.ml
+++ tester/evmTest.ml
@@ -58,6 +58,8 @@ let () =
   let x : Word256.word256 = Word256.W256 (true, []) in
   let open Big_int in
   let () = Printf.printf "hello %s\n" (string_of_big_int (big_int_of_word256 (word256_of_big_int (BatBig_int.big_int_of_int (333))))) in
-  let (_ : program_result) = program_sem dummy_variable_con dummy_constant_con 200 300 in
-  let () = Printf.printf "called EVM.\n" in
+  let (r : program_result) = program_sem dummy_variable_con dummy_constant_con 200 300 in
+  let f = format_program_result r in
+  let () = Easy_format.Pretty.to_stdout f in
+  let () = Printf.printf "\ncalled EVM.\n" in
   ()
