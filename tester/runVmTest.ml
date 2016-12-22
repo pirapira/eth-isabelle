52445ac7d8c66e80d3e27d7dddc2c9ed25f187db
Yoichi Hirai <i@yoichihirai.com>
2016-12-22 14:14:14 +0100

    Clear 001d67613a26fd3b19985fb50be0a8ba81e8ae72d97a6df55ac126462bf491bb test

--- tester/runVmTest.ml
+++ tester/runVmTest.ml
@@ -160,5 +160,6 @@ let () =
   let () = test_one_file "../tests/VMTests/vmArithmeticTest.json" in
   let () = test_one_file "../tests/VMTests/vmBitwiseLogicOperationTest.json" in
   let () = test_one_file "../tests/VMTests/vmPushDupSwapTest.json" in
+  let () = test_one_file "../tests/VMTests/vmInputLimitsLight.json" in
   let () = Printf.printf "all tests passed.\n" in
   ()
