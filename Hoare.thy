c9f24c8ce587b11f8dda2ee8d090fd9ebe0626cd
Yoichi Hirai <i@yoichihirai.com>
2017-01-02 23:50:14 +0100

    Starting something primitive

---
+++ Hoare.thy
@@ -0,0 +1,51 @@
+theory Hoare
+
+imports Main "./ContractSem"
+
+begin
+  
+(* With Hoare Triples, we cannot deal with programs that jump out *)
+(* However, as long as we stick to structured programming, it does not matter. *)
+
+(* position independent program representation *)
+
+  
+(* concatenation *)
+  
+(* if then else *)
+  
+definition if_then_else :: "int \<Rightarrow> inst list \<Rightarrow> inst list \<Rightarrow> inst list \<Rightarrow> inst list"
+where
+"if_then_else beginning cond then_case else_case =
+ cond
+ @ (* beginning + length cond *)
+ [Stack (PUSH_N (word_rsplit (word_of_int (beginning + int (length cond) + 8 + int (length else_case)) :: 16 word))), Pc JUMPI] 
+ @ (* beginning + length cond + 4 *)
+ else_case
+ @ (* beginning + length cond + length else_case + 4 *)
+ [Stack (PUSH_N (word_rsplit (word_of_int (beginning + int (length cond) + int (length else_case) + 9 + int (length then_case)) :: 16 word))), Pc JUMP]
+ @ (* beginning + length cond + length else_case + 8 *)
+ [Pc JUMPDEST]
+ @ (* beginning + length cond + length else_case + 9 *)
+ then_case
+ @ (* beginning + length cond + length else_case + 9 + length then_case *)
+ [Pc JUMPDEST]
+"
+
+(* example of if_then_else *)
+
+(* loop *)
+
+  
+(* precondition / post condition pair *)
+
+(* What would be the type of precondition? *)
+(* instruction_result \<Rightarrow> bool
+ * In the precondition, the program counter is overwritten.
+ *)
+
+(* validity of pre, program, post triples.
+ * Failures are considered as success.
+ *)
+
+end
