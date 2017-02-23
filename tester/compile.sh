d097d97df9f5c0ba88db134a222930e204ec312e
Sami Mäkelä <mrsmkl@gmail.com>
2017-02-23 20:29:38 +0200

    perhaps this will make tests work

--- tester/compile.sh
+++ tester/compile.sh
@@ -1,1 +1,4 @@
+rm -rf lem
+mkdir lem
+cp ../lem/*.ml lem
 BISECT_COVERAGE=YES ocamlbuild -use-ocamlfind -cflag -g -lflag -g -pkgs yojson,bignum,batteries jsonTest.native evmTest.native runVmTest.native kecTest.native
