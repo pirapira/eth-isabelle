c81a1d6eea623f0b847fdabb64a22a49a26c9faf
Sami Mäkelä <mrsmkl@gmail.com>
2017-03-23 21:33:58 +0200

    some tests now actually work

--- tester/stateTest.ml
+++ tester/stateTest.ml
@@ -51,7 +51,10 @@ let convert_state addr st = {
 }
 
 let make_state_list lst =
-  List.map (fun (a,st) -> let addr = Conv.word160_of_big_int (Big_int.big_int_of_string ("0x"^a)) in (addr, convert_state addr st)) lst
+  List.map (fun (a,st) ->
+     let addr = Conv.word160_of_big_int (Big_int.big_int_of_string ("0x"^a)) in
+     let stor_lst = List.map (fun (p,v) -> (Conv.word256_of_big_int p, string_to_w256 v)) st.Jsonparser.storage in
+     (addr, convert_state addr st, stor_lst)) lst
 
 let construct_tr a = {
   tr_from = Conv.word160_of_big_int a.address;
@@ -63,37 +66,64 @@ let construct_tr a = {
   tr_data = Conv.byte_list_of_hex_string a.data;
 }
 
+(*
+let debug_state = function
+ | Continue res ->
+   (match res.g_vmstate with
+   | InstructionContinue v ->
+   | _ -> () )
+ | _ -> ()
+*)
+
 let run_tr tr state block =
   let res = start_transaction tr state block in
   let rec do_run = function
    | Finished fi -> fi
-   | a -> do_run (next a) in
+   | a ->
+     do_run (next a) in
   let fi = do_run res in
   let final_state = end_transaction fi tr block in
   final_state
 
-let () =
-  let test : json = Yojson.Basic.from_file "../tests/StateTests/stExample.json" in
+let compare_storage a stor (p,v) =
+  if stor p <> v then begin
+      Printf.printf "address %s has storage %s at %s, but it should be %s!\n" (Conv.string_of_address a) 
+       (Conv.decimal_of_word256 (stor p)) (Conv.decimal_of_word256 p) (Conv.decimal_of_word256 v)
+  end
+
+let run_test (label, elm) =
+  let () = Printf.printf "%s\n%!" label in
+  let tc = parse_test_case elm in
+  let block_info = construct_block_info tc in
+  let tr = construct_tr tc.tr in
+  let pre_st = List.map (fun (a,b,_) -> (a,b)) (make_state_list tc.pre) in
+  let post_st = make_state_list tc.post in
+  let state x = try List.assoc x pre_st with _ -> empty_account0 x in
+  let state = run_tr tr state block_info in
+  List.iter (fun (a,cmp, storage_list) ->
+    let acc = state a in
+    if acc.account_balance0 <> cmp.account_balance0 then begin
+      Printf.printf "address %s has balance %s, but it should be %s!\n%!" (Conv.string_of_address a) (Conv.decimal_of_word256 acc.account_balance0)
+       (Conv.decimal_of_word256 cmp.account_balance0)
+    end;
+    if acc.account_nonce <> cmp.account_nonce then begin
+      Printf.printf "address %s has nonce %s, but it should be %s!\n%!" (Conv.string_of_address a) (Conv.decimal_of_word256 acc.account_nonce)
+       (Conv.decimal_of_word256 cmp.account_nonce)
+    end;
+    List.iter (compare_storage a acc.account_storage0) storage_list) post_st;
+  ()
+
+let main fn =
+  let test : json = Yojson.Basic.from_file fn in
   let test_assoc : (string * json) list = Util.to_assoc test in
   let () =
-    List.iter (fun (label, elm) ->
-        let () = Printf.printf "%s\n%!" label in
-        let tc = parse_test_case elm in
-        Printf.printf "address: %s\n" (bighex tc.tr.address);
-        let block_info = construct_block_info tc in
-        let tr = construct_tr tc.tr in
-        let pre_st = make_state_list tc.pre in
-        let post_st = make_state_list tc.post in
-        Printf.printf "state has %d accounts\n" (List.length pre_st);
-        let state x = try List.assoc x pre_st with _ -> empty_account0 x in
-        let state = run_tr tr state block_info in
-        List.iter (fun (a,cmp) ->
-           let acc = state a in
-           Printf.printf "address %s has balance %s\n" (Conv.string_of_address acc.account_address0) (Conv.decimal_of_word256 acc.account_balance0);
-           Printf.printf "should be %s\n" (Conv.decimal_of_word256 cmp.account_balance0)) post_st;
-        ()
-      ) test_assoc
+    List.iter run_test test_assoc
   in
   ()
+
+let _ =
+  for i = 1 to Array.length Sys.argv - 1 do
+    main Sys.argv.(i)
+  done
 
 
