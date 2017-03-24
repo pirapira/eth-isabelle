abeb2e00188eb00df29712a2c6de9a0928a111ac
Sami Mäkelä <mrsmkl@gmail.com>
2017-03-24 19:05:21 +0200

    more tests working

--- tester/stateTest.ml
+++ tester/stateTest.ml
@@ -113,17 +113,21 @@ let run_test (label, elm) =
     List.iter (compare_storage a acc.account_storage0) storage_list) post_st;
   ()
 
-let main fn =
+let main fn filter =
   let test : json = Yojson.Basic.from_file fn in
   let test_assoc : (string * json) list = Util.to_assoc test in
+  let check a = match filter with None -> true | Some b -> a = b in
   let () =
-    List.iter run_test test_assoc
+    List.iter (fun (a,b) -> if check a then run_test (a,b)) test_assoc
   in
   ()
 
 let _ =
+  if Array.length Sys.argv = 2 then main Sys.argv.(1) None else
+  if Array.length Sys.argv > 2 then main Sys.argv.(1) (Some Sys.argv.(2))
+  (*
   for i = 1 to Array.length Sys.argv - 1 do
     main Sys.argv.(i)
-  done
+  done*)
 
 
