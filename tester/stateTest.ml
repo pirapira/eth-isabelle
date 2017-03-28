97357e06b174b220eb62112f474a862498fcf002
Sami Mäkelä <mrsmkl@gmail.com>
2017-03-28 10:59:13 +0300

    original tests are now working

--- tester/stateTest.ml
+++ tester/stateTest.ml
@@ -66,7 +66,8 @@ let construct_tr a = {
   tr_data = Conv.byte_list_of_hex_string a.data;
 }
 
-let w256hex i = Z.format "%d" (Word256.word256ToNatural i)
+let w256hex i = Z.format "%x" (Word256.word256ToNatural i)
+let w256dec i = Z.format "%d" (Word256.word256ToNatural i)
 
 let debug_vm c1 pr =  
  match pr with
@@ -75,7 +76,7 @@ let debug_vm c1 pr =
      (match vctx_next_instruction v c1 with
       | None -> ()
       | Some i ->
-        prerr_endline ("Watch " ^ w256hex (v.vctx_storage (Word256.word256FromNat 1)));
+        (* prerr_endline ("Watch " ^ w256hex (v.vctx_storage (Word256.word256FromNat 1))); *)
         (* prerr_endline ("Calldata " ^ String.concat "," (List.map (fun x -> Z.format "%x" (Word8.word8ToNatural x)) v.vctx_data_sent)); *)
         prerr_endline ("Inst " ^ String.concat "," (List.map (fun x -> Z.format "%x" (Word8.word8ToNatural x)) (inst_code i)));
         prerr_endline ("Stack " ^ String.concat "," (List.map (fun x -> Z.format "%d" (Word256.word256ToNatural x)) v.vctx_stack)) )
@@ -96,6 +97,10 @@ let run_tr tr state block =
      if debug_mode then debug_state a;
      do_run (next a) in
   let fi = do_run res in
+  if debug_mode then begin
+    prerr_endline ("Bal " ^ w256dec (fi.f_state tr.tr_from).account_balance0);
+    prerr_endline ("Killed " ^ string_of_int (List.length fi.f_killed));
+  end;
   let final_state = end_transaction fi tr block in
   final_state
 
