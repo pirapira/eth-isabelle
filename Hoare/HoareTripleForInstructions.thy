c07ec23fc85e4f8e7af25d0cb95fe55b71ed586a
Yoichi Hirai <i@yoichihirai.com>
2017-04-25 18:49:11 +0200

    Fix build

--- Hoare/HoareTripleForInstructions.thy
+++ Hoare/HoareTripleForInstructions.thy
@@ -1865,7 +1865,7 @@ lemma stack_height_after_call [simp] :
         (StackHeightElm (h + 7) \<in>
           instruction_result_as_set co_ctx (InstructionContinue x1)) \<Longrightarrow>
         (StackHeightElm h
-          \<in> instruction_result_as_set co_ctx (subtract_gas g memu (call x1 co_ctx)))
+          \<in> instruction_result_as_set co_ctx (subtract_gas g memu (call net x1 co_ctx)))
         "
 apply(simp add: call_def)
 apply(case_tac "vctx_stack x1"; simp)
@@ -1959,7 +1959,7 @@ lemma call_new_balance [simp] :
        vctx_stack x1 = g # r # v # in_begin # in_size # out_begin # out_size # tf \<Longrightarrow>
        BalanceElm (this, fund - v)
        \<in> instruction_result_as_set co_ctx
-           (subtract_gas (meter_gas (Misc CALL) x1 co_ctx net) memu (call x1 co_ctx))"
+           (subtract_gas (meter_gas (Misc CALL) x1 co_ctx net) memu (call net x1 co_ctx))"
 apply(clarify)
 apply(auto simp add: call_def failed_for_reasons_def)
 apply(simp add: instruction_result_as_set_def subtract_gas.simps strict_if_def)
@@ -2261,7 +2261,7 @@ begin
 lemma call_memory_no_change [simp] :
   "x \<in> memory_range_elms in_begin input \<Longrightarrow>
    x \<in> instruction_result_as_set co_ctx
-         (subtract_gas (meter_gas (Misc CALL) x1 co_ctx net) memu (call x1 co_ctx)) =
+         (subtract_gas (meter_gas (Misc CALL) x1 co_ctx net) memu (call net x1 co_ctx)) =
   (x \<in> instruction_result_as_set co_ctx (InstructionContinue x1))"
 apply(simp add: call_def)
 apply(case_tac "vctx_stack x1"; simp)
@@ -2276,7 +2276,7 @@ done
 
 lemma memory_call [simp] :
   "x \<in> memory_range_elms in_begin input \<Longrightarrow>
-    x \<in> instruction_result_as_set co_ctx (call x1 co_ctx) =
+    x \<in> instruction_result_as_set co_ctx (call net x1 co_ctx) =
     (x \<in> instruction_result_as_set co_ctx (InstructionContinue x1))"
 apply(simp add: call_def)
 apply(case_tac "vctx_stack x1"; simp)
