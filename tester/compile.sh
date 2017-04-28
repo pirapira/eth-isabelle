031768bfe8f9c8cd7030e4d75ad2eb1b3761605b
Yoichi Hirai <i@yoichihirai.com>
2017-04-28 12:26:01 +0200

    Add an executable runBlockchainTest.native

--- tester/compile.sh
+++ tester/compile.sh
@@ -2,4 +2,4 @@ cd ..; make lem-ocaml; cd tester
 rm -rf lem
 mkdir lem
 cp ../lem/*.ml lem
-BISECT_COVERAGE=YES ocamlbuild -use-ocamlfind -cflag -g -lflag -g -pkgs yojson,bignum,batteries,ecc jsonTest.native evmTest.native runVmTest.native kecTest.native stateTest.native ecdsademo.native stateTestReturnStatus.native
+BISECT_COVERAGE=YES ocamlbuild -use-ocamlfind -cflag -g -lflag -g -pkgs yojson,bignum,batteries,ecc jsonTest.native evmTest.native runVmTest.native kecTest.native stateTest.native ecdsademo.native stateTestReturnStatus.native runBlockchainTest.native
---
+++ tester/runBlockchainTest.ml
@@ -0,0 +1,2 @@
+let _ =
+  Printf.printf "blockchaintest.\n"
