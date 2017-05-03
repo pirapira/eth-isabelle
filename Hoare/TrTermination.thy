d72a0f3f58c5b76cae7bc589e6bb798d9d08f8a9
Sami Mäkelä <mrsmkl@gmail.com>
2017-05-03 15:04:23 +0300

    call and callcode gas are now good, hopefully delegate will work soon

--- Hoare/TrTermination.thy
+++ Hoare/TrTermination.thy
@@ -152,7 +152,8 @@ lemma did_not_delegate :
  False"
 by (auto simp:instruction_sem_simps Let_def split:split_inst)
 
-lemma word_convert_compare : "x \<ge> 0 \<Longrightarrow> uint (word_of_int x) \<le> x"
+lemma word_convert_compare :
+   "x \<ge> 0 \<Longrightarrow> uint (word_of_int x :: w256) \<le> x"
   by (simp add: int_word_uint zmod_le_nonneg_dividend)
 
 lemma cmem_new :
@@ -217,14 +218,13 @@ by force
 
 lemma call_gas_compare :
 "vctx_memory_usage v1 \<ge> 0 \<Longrightarrow>
- vctx_recipient v1 c = ucast x21a \<Longrightarrow>
  vctx_stack v1 = x21 # x21a # x21b # x21c # x21d # x21e # x21f # x22f \<Longrightarrow>
- Ccallgas x21 x21a x21b (\<not> vctx_account_existence v1 (ucast x21a))
+ Ccallgas x21 x21a x21b (\<not> vctx_account_existence v1 (vctx_recipient v1 c))
                 (vctx_gas v1) net
                 (calc_memu_extra (vctx_memory_usage v1) x21 x21a x21b x21c x21d x21e
                   x21f)
        < meter_gas (Misc CALL) v1 c net"
-by (auto simp add:gas_simps  Ccallgas_def meter_gas_def C_def
+by (simp add:gas_simps  Ccallgas_def meter_gas_def C_def
   thirdComponentOfC_def Gcallstipend_def vctx_stack_default_def
   uint_nat cmem_new_helper  cmem_new_helper2
 calc_memu_extra_def min_L_helper min_L_helper2)
@@ -237,16 +237,96 @@ by (simp add:calc_memu_extra_def cmem_new)
 lemma callgas_ge0 :
   "memu \<ge> 0 \<Longrightarrow>
    Ccallgas x21 x21a x21b aex gas1 net memu \<ge> 0"
-apply (auto simp add:gas_simps  Ccallgas_def meter_gas_def C_def
+by (auto simp add:gas_simps  Ccallgas_def meter_gas_def C_def
   thirdComponentOfC_def Gcallstipend_def vctx_stack_default_def
   cmem_new_helper  cmem_new_helper2
 calc_memu_extra_def min_L_helper min_L_helper2 L_def)
 
+lemma aux :
+ "x < y \<Longrightarrow> x \<ge> 0 \<Longrightarrow> uint (word_of_int x :: w256) < y"
+using word_convert_compare [of x]
+by auto
+
 lemma call_system_gas :
 "instruction_sem v1 c (Misc CALL) net =
  InstructionToEnvironment (ContractCall args) v2 x33 \<Longrightarrow>
+  vctx_next_instruction v1 c = Some (Misc CALL) \<Longrightarrow>
+ vctx_memory_usage v1 \<ge> 0 \<Longrightarrow>
  vctx_gas v1 > vctx_gas v2 + uint (callarg_gas args)"
 apply (auto simp:instruction_sem_simps Let_def split:split_inst)
+apply (rule aux)
+apply (rule call_gas_compare, auto)
+apply (simp add:vctx_recipient_def vctx_next_instruction_default_def
+  vctx_stack_default_def)
+using callgas_ge0 memu_extra_ge0 apply force
+done
+
+lemma memu_code :
+"new_memory_consumption (Misc CALLCODE)
+         memu x21 x21a x21b x21c
+         x21d x21e x21f =
+new_memory_consumption (Misc CALL)
+         memu x21 x21a x21b x21c
+         x21d x21e x21f"
+by (simp add:new_memory_consumption.simps)
+
+lemma callcode_gas_compare :
+"vctx_memory_usage v1 \<ge> 0 \<Longrightarrow>
+ vctx_stack v1 = x21 # x21a # x21b # x21c # x21d # x21e # x21f # x22f \<Longrightarrow>
+ Ccallgas x21 x21a x21b (\<not> vctx_account_existence v1 (vctx_recipient v1 c))
+        (vctx_gas v1) net
+        (calc_memu_extra (vctx_memory_usage v1) x21 x21a x21b x21c x21d x21e x21f)
+       < meter_gas (Misc CALLCODE) v1 c net"
+by (simp add:gas_simps  Ccallgas_def meter_gas_def C_def
+  thirdComponentOfC_def Gcallstipend_def vctx_stack_default_def
+  uint_nat cmem_new_helper  cmem_new_helper2
+calc_memu_extra_def min_L_helper min_L_helper2 memu_code)
+
+lemma callcode_system_gas :
+"instruction_sem v1 c (Misc CALLCODE) net =
+ InstructionToEnvironment (ContractCall args) v2 x33 \<Longrightarrow>
+ vctx_next_instruction v1 c = Some (Misc CALLCODE) \<Longrightarrow>
+ vctx_memory_usage v1 \<ge> 0 \<Longrightarrow>
+ vctx_gas v1 > vctx_gas v2 + uint (callarg_gas args)"
+apply (auto simp:instruction_sem_simps Let_def split:split_inst)
+apply (rule aux)
+apply (rule callcode_gas_compare, auto)
+apply (simp add:vctx_recipient_def vctx_next_instruction_default_def
+  vctx_stack_default_def)
+using callgas_ge0 memu_extra_ge0 apply force
+done
+
+
+lemma delegate_gas_compare :
+ "\<not> before_homestead net \<Longrightarrow>
+  vctx_memory_usage v1 \<ge> 0 \<Longrightarrow>
+  vctx_stack v1 = x21 # x21a # x21b # x21c # x21d # x21e # x22e \<Longrightarrow>
+  Ccallgas x21 x21a 0
+        (\<not> vctx_account_existence v1 (vctx_recipient v1 c))
+        (vctx_gas v1) net
+        (calc_memu_extra2 v1 x21 x21a x21b x21c
+          x21d x21e 0)
+       < meter_gas (Misc DELEGATECALL) v1 c net"
+apply (cases x22e)
+apply (auto simp add:gas_simps  Ccallgas_def meter_gas_def C_def
+  thirdComponentOfC_def Gcallstipend_def vctx_stack_default_def
+  uint_nat cmem_new_helper  cmem_new_helper2
+calc_memu_extra2_def min_L_helper min_L_helper2)
+
+lemma delegate_system_gas :
+"instruction_sem v1 c (Misc DELEGATECALL) net =
+ InstructionToEnvironment (ContractDelegateCall args) v2 x33 \<Longrightarrow>
+ vctx_next_instruction v1 c = Some (Misc DELEGATECALL) \<Longrightarrow>
+ vctx_memory_usage v1 \<ge> 0 \<Longrightarrow>
+ vctx_gas v1 > vctx_gas v2 + uint (callarg_gas args)"
+apply (auto simp:instruction_sem_simps Let_def split:split_inst)
+apply (rule aux)
+apply (rule callcode_gas_compare, auto)
+apply (simp add:vctx_recipient_def vctx_next_instruction_default_def
+  vctx_stack_default_def)
+ussing callgas_ge0 memu_extra_ge0 apply force
+done
+
 
 
 (* calls without value (do not increase gas) *)
