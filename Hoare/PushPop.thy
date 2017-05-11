0d2184bb22ab6eb07956f656b6a785de87dbea1e
Sami Mäkelä <mrsmkl@gmail.com>
2017-05-11 19:02:00 +0300

    finding returns

--- Hoare/PushPop.thy
+++ Hoare/PushPop.thy
@@ -162,7 +162,6 @@ by auto
 lemma stack_unchanged :
   "iterR k r st1 st2 \<Longrightarrow>
    push_pop r f \<Longrightarrow>
-   urel r \<Longrightarrow>
    (\<forall>sti. \<forall>k1 \<le> k. iterR k1 r st1 sti \<longrightarrow>
                    l \<le> length (f sti) ) \<Longrightarrow>
    takeLast l (f st2) = takeLast l (f st1)"
@@ -171,25 +170,103 @@ apply auto
 subgoal for k st3 z
 apply (rule stack_unchanged_split [of k r st3 st2 l f])
 apply auto
-apply (rule ex_and)
-  apply blast
+apply (rule exI [where x=z])
 apply auto
-defer
-apply (rule next_unchanged [of r f], auto)
-subgoal for st
-by (rule funny[of k r st3 l f st]; auto)
-apply (drule spec2[where x="st3" and y = "0"], auto)
-subgoal for st
-apply (rule Spec[where R="\<lambda>st1. iterR k r st1 st2"
+apply (drule Spec[where R="\<lambda>st1. iterR k r st1 st2"
    and P="\<lambda>st1. \<forall>sti k1. k1 \<le> k \<longrightarrow> iterR k1 r st1 sti \<longrightarrow> l \<le> length (f sti)"
-   and x="st"])
-apply auto
-defer
+   and x="z"], auto)
 apply fastforce
-  using apply_urel by fastforce
+apply (rule next_unchanged [of r f], auto)
+apply (drule spec2[where x="z" and y = "Suc 0"], auto)
+apply (drule spec2[where x="st3" and y = "0"], auto)
 done done
 
 (* find returns *)
+
+lemma what :
+   "length (f st2) \<le> length (f z) - Suc 0 \<Longrightarrow>
+    length (f st2) \<le> length (f z)"
+by auto
+
+lemma find_return_aux :
+"( iterR k r z st2 \<Longrightarrow>
+        length (f st2) \<le> length (f z) \<Longrightarrow>
+        \<exists>st3 k3.
+           k3 \<le> k \<and>
+           iterR k3 r z st3 \<and>
+           f st3 =
+           takeLast (length (f st2)) (f z)) \<Longrightarrow>
+    push_pop r f \<Longrightarrow>
+    length (f st2) \<le> length (f st) \<Longrightarrow>
+    r st z \<Longrightarrow>
+    iterR k r z st2 \<Longrightarrow>
+    \<exists>st3 k3.
+       k3 \<le> Suc k \<and>
+       iterR k3 r st st3 \<and>
+       f st3 = takeLast (length (f st2)) (f st)"
+apply (cases "f st = f z")
+apply auto
+apply force
+apply (cases "f st = tl (f z)")
+apply auto
+subgoal
+using what [of f st2 z]
+apply auto
+  using iterR.simps(2) next_unchanged apply fastforce
+done
+apply (cases "f z = tl (f st)")
+apply auto
+apply (cases "length (f st2) = length (f st)")
+apply auto
+  apply (metis (mono_tags) diff_zero drop_0 iterR.simps(1) le0 rev_drop rev_swap takeLast_def)
+subgoal proof -
+  assume a1: "length (f st2) \<le> length (f st) - Suc 0 \<Longrightarrow> \<exists>st3 k3. k3 \<le> k \<and> iterR k3 r z st3 \<and> f st3 = takeLast (length (f st2)) (tl (f st))"
+  assume a2: "push_pop r f"
+  assume a3: "length (f st2) \<le> length (f st)"
+  assume a4: "r st z"
+  assume a5: "f st \<noteq> tl (f st)"
+  assume a6: "f z = tl (f st)"
+  assume a7: "length (f st2) \<noteq> length (f st)"
+  obtain nn :: nat and aa :: 'a where
+    f8: "\<not> length (f st2) \<le> length (f st) - Suc 0 \<or> nn \<le> k \<and> iterR nn r z aa \<and> takeLast (length (f st2)) (tl (f st)) = f aa"
+    using a1 by (metis (no_types))
+  then have f9: "\<not> length (f st2) \<le> length (f st) - 1 \<or> takeLast (length (f st2)) (f z) = f aa"
+    using a6 One_nat_def by presburger
+  have f10: "length (f st) - 1 = length (f z) \<or> [] = f st"
+    using a6 by auto
+  have f11: "length (f st2) \<le> length (f z) \<or> [] = f st"
+    using a7 a6 a3 by (metis (no_types) Nitpick.size_list_simp(2) le_antisym not_less_eq_eq)
+  then have f12: "iterR nn r z aa \<or> [] = f st"
+    using f10 f8 One_nat_def by presburger
+  have f13: "takeLast (length (f st2)) (f z) = f aa \<or> [] = f st"
+    using f11 f10 f9 by presburger
+  have f14: "nn \<le> k \<or> [] = f st"
+    using f11 f10 f8 One_nat_def by presburger
+  have f15: "takeLast (length (f st2)) (f st) = takeLast (length (f st2)) (f z) \<or> [] = f st"
+    using f11 a4 a3 a2 by (metis next_unchanged)
+  have "[] \<noteq> f st"
+    using a5 by (metis list.sel(2))
+  then have "\<exists>n a. iterR (Suc n) r st a \<and> takeLast (length (f st2)) (f st) = f a \<and> \<not> Suc k \<le> n"
+    using f15 f14 f13 f12 a4 not_less_eq_eq by auto
+  then show ?thesis
+    by (metis not_less_eq_eq)
+qed
+apply (metis push_pop_def)
+done
+
+lemma find_return :
+  "iterR k r st1 st2 \<Longrightarrow>
+   push_pop r f \<Longrightarrow>
+   length (f st2) \<le> length (f st1) \<Longrightarrow>
+   \<exists>st3 k3. k3 \<le> k \<and> iterR k3 r st1 st3 \<and>
+   f st3 = takeLast (length (f st2)) (f st1)"
+apply (induction k arbitrary: st1)
+  apply (simp add: takeLast_def)
+apply auto
+subgoal for k st z
+using find_return_aux [of k r z st2 f st]
+by fastforce
+done
 
 (* monotonic invariants *)
 
