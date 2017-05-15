0bd2194702726fc17630ca996013dfb7dcacc86c
Sami Mäkelä <mrsmkl@gmail.com>
2017-05-15 12:44:04 +0300

    old proofs now working

--- Hoare/PathRel.thy
+++ Hoare/PathRel.thy
@@ -70,182 +70,38 @@ lemma push_pop_inc_dec_map : "mapR push_pop length \<subseteq> inc_dec"
 unfolding mapR_def
 using push_pop_inc_dec by fastforce
 
-lemma list_all_value :
-   "inc_decL lst \<Longrightarrow>
-    length lst > 0 \<Longrightarrow>
-    min (hd lst) (last lst) \<le> x \<Longrightarrow>
-    x \<le> max (hd lst) (last lst) \<Longrightarrow>
-    \<exists>i. lst!i = x"
-apply (induction lst)
-apply (auto simp add:inc_decL_def)
-apply (case_tac lst)
-apply auto
-  apply (meson nth_Cons_0)
-apply (auto simp: inc_dec_def sucR_def)
-  apply (meson nth_Cons_Suc)
-subgoal proof -
-  fix aa :: nat and list :: "nat list"
-  assume a1: "\<lbrakk>min aa (if list = [] then aa else last list) \<le> x; x \<le> max aa (if list = [] then aa else last list)\<rbrakk> \<Longrightarrow> \<exists>i. (aa # list) ! i = x"
-  assume a2: "min (Suc aa) (if list = [] then aa else last list) \<le> x"
-  assume a3: "x \<le> max (Suc aa) (if list = [] then aa else last list)"
-  obtain nn :: nat where
-    f4: "\<not> min aa (if list = [] then aa else last list) \<le> x \<or> \<not> x \<le> max aa (if list = [] then aa else last list) \<or> (aa # list) ! nn = x"
-    using a1 by metis
-  have f5: "Suc aa \<le> (if list = [] then aa else last list) \<or> x \<le> Suc aa"
-    using a3 by (metis max_def)
-  have f6: "\<forall>n. min n (min (Suc aa) (if list = [] then aa else last list)) \<le> x"
-    using a2 by (metis min.coboundedI2)
-  have f7: "\<not> Suc aa \<le> (if list = [] then aa else last list) \<or> Suc aa \<le> x"
-    using a2 by linarith
-  have f8: "Suc aa \<le> aa \<or> list \<noteq> [] \<or> x \<le> Suc aa"
-    using a3 by force
-  have f9: "\<forall>n. \<not> n \<le> min (Suc aa) (if list = [] then aa else last list) \<or> n \<le> x"
-    using f6 by (metis min_def)
-  have f10: "(if list = [] then aa else last list) \<le> aa \<or> x \<le> (if list = [] then aa else last list)"
-    using a3 by linarith
-  have f11: "(if list = [] then aa else last list) \<le> x \<or> aa \<le> (if list = [] then aa else last list)"
-    using a2 by linarith
-  { assume "Suc (last list) \<le> aa"
-    then have "\<not> aa \<le> last list"
-      by (metis not_less_eq_eq)
-    moreover
-    { assume "\<exists>n. \<not> aa \<le> last list \<and> \<not> min n (last list) \<le> x"
-      then have "\<not> aa \<le> (if False then aa else last list) \<and> \<not> last list \<le> x"
-        by (meson min.coboundedI2)
-      then have "list = []"
-        using f11 by presburger }
-    ultimately have "x \<le> aa \<longrightarrow> x \<le> aa \<and> min aa (if list = [] then aa else last list) \<le> x \<and> \<not> aa \<le> (if list = [] then aa else last list) \<or> list = []"
-      by presburger }
-  moreover
-  { assume "\<not> Suc (if False then aa else last list) \<le> aa"
-    moreover
-    { assume "\<not> Suc (if list = [] then aa else last list) \<le> aa"
-      moreover
-      { assume "\<exists>n. \<not> min aa n \<le> x"
-        moreover
-        { assume "Suc aa \<le> last list"
-          then have "x \<le> aa \<longrightarrow> list = []"
-            using f7 not_less_eq_eq by presburger }
-        ultimately have "x \<le> aa \<longrightarrow> x \<le> aa \<and> min aa (if list = [] then aa else last list) \<le> x \<and> \<not> aa \<le> (if list = [] then aa else last list) \<or> list = []"
-          using f9 by force }
-      moreover
-      { assume "min aa (if list = [] then aa else last list) \<le> x \<and> \<not> Suc (if list = [] then aa else last list) \<le> aa"
-        moreover
-        { assume "\<exists>n. \<not> n \<le> aa \<and> n \<le> x"
-          then have "\<not> x \<le> aa"
-            by (metis min.coboundedI2 min_def) }
-        ultimately have "x \<le> aa \<longrightarrow> aa \<le> (if list = [] then aa else last list) \<and> min aa (if list = [] then aa else last list) \<le> x \<and> x \<le> (if list = [] then aa else last list)"
-          by (meson not_less_eq_eq) }
-      ultimately have "x \<le> aa \<longrightarrow> aa \<le> (if list = [] then aa else last list) \<and> min aa (if list = [] then aa else last list) \<le> x \<and> x \<le> (if list = [] then aa else last list) \<or> x \<le> aa \<and> min aa (if list = [] then aa else last list) \<le> x \<and> \<not> aa \<le> (if list = [] then aa else last list) \<or> list = []"
-        by blast }
-    ultimately have "x \<le> aa \<longrightarrow> aa \<le> (if list = [] then aa else last list) \<and> min aa (if list = [] then aa else last list) \<le> x \<and> x \<le> (if list = [] then aa else last list) \<or> x \<le> aa \<and> min aa (if list = [] then aa else last list) \<le> x \<and> \<not> aa \<le> (if list = [] then aa else last list) \<or> list = []"
-      by force }
-  moreover
-  { assume "\<not> x \<le> aa"
-    then have "list = [] \<or> aa \<le> (if list = [] then aa else last list) \<and> min aa (if list = [] then aa else last list) \<le> x \<and> x \<le> (if list = [] then aa else last list) \<or> (\<exists>n. x = n \<and> \<not> n \<le> aa \<and> n \<le> Suc aa)"
-      using f10 f5 by (metis (no_types) min.coboundedI1 nat_le_linear not_less_eq_eq) }
-  moreover
-  { assume "list = []"
-    moreover
-    { assume "x \<le> aa \<and> min aa (if list = [] then aa else last list) \<le> x \<and> list = []"
-      moreover
-      { assume "aa \<le> aa \<and> list = [] \<and> min aa (if list = [] then aa else last list) \<le> x"
-        then have "x \<le> max aa (if True then aa else last list) \<and> min aa (if list = [] then aa else last list) \<le> x \<and> list = [] \<or> (\<exists>n. x = n \<and> \<not> n \<le> aa \<and> n \<le> Suc aa)"
-          using f8 by force }
-      ultimately have "(\<exists>n. x = n \<and> \<not> n \<le> aa \<and> n \<le> Suc aa) \<or> (\<exists>n. (aa # list) ! n = x)"
-        using f4 by force }
-    ultimately have "(\<exists>n. x = n \<and> \<not> n \<le> aa \<and> n \<le> Suc aa) \<or> (\<exists>n. (aa # list) ! n = x)"
-      using a3 a2 by force }
-  moreover
-  { assume "\<exists>n. x = n \<and> \<not> n \<le> aa \<and> n \<le> Suc aa"
-    then have "\<exists>n. (Suc aa # aa # list) ! n = x"
-      by (metis le_SucE nth_Cons_0) }
-  ultimately show "\<exists>n. (Suc aa # aa # list) ! n = x"
-    using f4 by (metis max_def nth_Cons_Suc)
-qed
-proof -
-  fix a :: nat and list :: "nat list"
-  assume a1: "min a (if list = [] then Suc a else last list) \<le> x"
-  assume a2: "x \<le> max a (if list = [] then Suc a else last list)"
-  assume a3: "\<lbrakk>min (Suc a) (if list = [] then Suc a else last list) \<le> x; x \<le> max (Suc a) (if list = [] then Suc a else last list)\<rbrakk> \<Longrightarrow> \<exists>i. (Suc a # list) ! i = x"
-  have f4: "\<And>n. min x n \<le> max a (if list = [] then Suc a else last list)"
-    using a2 by (metis min.coboundedI1)
-  then have f5: "\<And>n. x \<le> n \<or> n \<le> max a (if list = [] then Suc a else last list)"
-    by (metis min_def)
-  then have f6: "x \<le> Suc (max a (if list = [] then Suc a else last list))"
-    by (metis not_less_eq_eq order_refl)
-  obtain nn :: nat where
-    f7: "\<not> x \<le> max (Suc a) (if list = [] then Suc a else last list) \<or> \<not> min (Suc a) (if list = [] then Suc a else last list) \<le> x \<or> (Suc a # list) ! nn = x"
-    using a3 by blast
-  then have f8: "\<not> x \<le> max (Suc a) (if list = [] then Suc a else last list) \<or> \<not> (if list = [] then Suc a else last list) \<le> x \<or> Suc a \<le> (if list = [] then Suc a else last list) \<or> (Suc a # list) ! nn = x"
-    by (metis min_def)
-  then have f9: "\<not> x \<le> Suc a \<or> \<not> (if list = [] then Suc a else last list) \<le> x \<or> Suc a \<le> (if list = [] then Suc a else last list) \<or> Suc a \<le> (if list = [] then Suc a else last list) \<or> (Suc a # list) ! nn = x"
-    by (metis max_def)
-  have f10: "\<not> Suc a \<le> x \<or> \<not> x \<le> Suc a \<or> Suc a \<le> (if list = [] then Suc a else last list) \<or> (Suc a # list) ! nn = x"
-    using f7 by (metis (no_types) max_def min.coboundedI1)
-  have f11: "\<not> Suc a \<le> (if list = [] then Suc a else last list) \<or> \<not> Suc a \<le> x \<or> \<not> x \<le> (if list = [] then Suc a else last list) \<or> (Suc a # list) ! nn = x"
-    using f7 by (metis (no_types) max_def min.coboundedI1)
-  { assume "x \<noteq> a"
-    then have "Suc x \<noteq> Suc a"
-      by blast
-    moreover
-    { assume "Suc a \<le> x"
-      moreover
-      { assume "x \<le> Suc a \<and> Suc a \<le> x"
-        then have "(if list = [] then Suc a else last list) \<le> a \<and> \<not> a \<le> (if list = [] then Suc a else last list) \<or> Suc a \<le> (if list = [] then Suc a else last list) \<and> Suc a \<le> x \<and> x \<le> (if list = [] then Suc a else last list) \<or> Suc a \<le> x \<and> x \<le> Suc a \<and> \<not> Suc a \<le> (if list = [] then Suc a else last list)"
-          using a2 by (metis (no_types) max_def not_less_eq_eq)
-        then have "(if list = [] then Suc a else last list) \<le> a \<and> \<not> a \<le> (if list = [] then Suc a else last list) \<or> (\<exists>n. (Suc a # list) ! n = x)"
-          using f11 f10 by blast }
-      moreover
-      { assume "\<not> x \<le> Suc a"
-        moreover
-        { assume "\<not> (if list = [] then Suc a else last list) \<le> a \<and> \<not> x \<le> Suc a"
-          then have "Suc a \<le> (if list = [] then Suc a else last list) \<and> \<not> x \<le> Suc a"
-            by (metis not_less_eq_eq)
-          moreover
-          { assume "Suc a \<le> (if list = [] then Suc a else last list) \<and> Suc a \<le> (if list = [] then Suc a else last list) \<and> x \<le> (if list = [] then Suc a else last list) \<and> \<not> x \<le> Suc a"
-            then have "x \<le> max (Suc a) (if list = [] then Suc a else last list) \<and> min (Suc a) (if list = [] then Suc a else last list) \<le> x"
-              by linarith
-            then have "\<exists>n. (Suc a # list) ! n = x"
-              using f7 by metis }
-          ultimately have "(if list = [] then Suc a else last list) \<le> a \<and> \<not> a \<le> (if list = [] then Suc a else last list) \<or> (\<exists>n. (Suc a # list) ! n = x)"
-            using f4 by (metis (no_types) max_def min_def) }
-        ultimately have "(if list = [] then Suc a else last list) \<le> a \<and> \<not> a \<le> (if list = [] then Suc a else last list) \<or> (\<exists>n. (Suc a # list) ! n = x)"
-          using f5 by (metis (no_types) max_def not_less_eq_eq) }
-      ultimately have "\<exists>n. (Suc a # list) ! n = x"
-        using f9 f6 a1 by (metis (no_types) max_def min_def not_less_eq_eq) }
-    moreover
-    { assume "\<not> Suc a \<le> x \<and> Suc x \<noteq> Suc a"
-      then have "Suc x \<le> a"
-        by (meson le_SucE not_less_eq_eq)
-      then have "Suc x \<le> a \<and> list \<noteq> []"
-        using a1 min_def by auto
-      then have "last list \<le> a \<and> \<not> a \<le> last list \<and> list \<noteq> []"
-        using a1 by force
-      then have "x \<le> max (Suc a) (last list) \<and> last list \<le> x \<and> list \<noteq> [] \<and> \<not> Suc a \<le> last list"
-        using f6 a1 by (simp add: max_def min_def)
-      then have "\<exists>n. (Suc a # list) ! n = x"
-        using f8 by auto }
-    ultimately have "\<exists>n. (a # Suc a # list) ! n = x"
-      by (metis (full_types) nth_Cons_Suc) }
-  then show "\<exists>n. (a # Suc a # list) ! n = x"
-    by (metis (no_types) nth_Cons_0)
-qed
-
 definition hd_last :: "'a list \<Rightarrow> 'a \<Rightarrow> 'a \<Rightarrow> bool" where
 "hd_last lst a b = (hd lst = a \<and> last lst = b \<and> length lst > 0)"
+
+lemma converse_rev : "pathR r lst \<Longrightarrow> pathR (converse r) (rev lst)"
+unfolding path_defs path_def
+  by (smt Suc_diff_Suc Suc_eq_plus1_left add.commute add.right_neutral converse.intros diff_Suc_less le_less_trans length_rev less_diff_conv not_add_less1 not_less rev_nth)
+
+lemma sym_rev : "sym r \<Longrightarrow> pathR r lst \<Longrightarrow> pathR r (rev lst)"
+  by (metis converse_rev sym_conv_converse_eq)
 
 lemma list_all_values :
    "inc_decL lst \<Longrightarrow>
     length lst > 0 \<Longrightarrow>
-    min (hd lst) (last lst) \<le> x \<Longrightarrow>
-    x \<le> max (hd lst) (last lst) \<Longrightarrow>
-    x \<in> set lst"
+    last lst \<le> hd lst \<Longrightarrow>
+    {last lst .. hd lst} \<subseteq> set lst"
 apply (induction lst)
 apply (auto simp add:inc_decL_def inc_dec_def sucR_def)
-apply (case_tac lst; auto)
-  using le_less min_le_iff_disj sup.cobounded2 sup_nat_def apply fastforce
-  by fastforce
+apply (case_tac lst; auto; fastforce)
+done
+
+lemma sym_inc_dec : "sym inc_dec"
+  by (simp add: inc_dec_def sup_assoc sym_Id sym_Un sym_Un_converse)
+
+
+lemma list_all_values2 :
+   "inc_decL lst \<Longrightarrow>
+    length lst > 0 \<Longrightarrow>
+    {min (hd lst) (last lst) .. max (hd lst) (last lst)} \<subseteq> set lst"
+apply (cases "last lst \<le> hd lst")
+  using list_all_values apply fastforce
+  using list_all_values [of "rev lst"]
+  by (simp add: sym_rev hd_rev inc_decL_def sym_inc_dec last_rev max_def min_def)
 
 definition takeLast :: "nat \<Rightarrow> 'a list \<Rightarrow> 'a list" where
 "takeLast n lst = rev (take n (rev lst))"
@@ -297,5 +153,155 @@ apply (case_tac lst; auto)
 apply (auto simp:pathR.simps)
   apply (smt Nitpick.size_list_simp(2) PathRel.take_all basic_trans_rules(31) inf_sup_aci(5) le_SucE list.sel(3) mem_Collect_eq next_unchanged prod.sel(1) prod.sel(2) push_pop_def sup.cobounded2 tlR_def zero_order(2))
   by (smt Suc_leD Suc_leI inf_sup_aci(5) inf_sup_ord(3) le_imp_less_Suc length_Cons mem_Collect_eq next_unchanged prod.inject push_pop_def subset_eq tlR_def)
+
+definition monoI :: "('a \<Rightarrow> bool) \<Rightarrow> ('a * 'a list) \<Rightarrow> bool" where
+"monoI iv v = (\<forall>i < length (snd v). iv (snd v!i) \<longrightarrow> iv ((fst v#snd v)!i))"
+
+definition mono_same :: "('a \<Rightarrow> bool) \<Rightarrow> ('a * 'a list) rel" where
+"mono_same iv = {((g1,lst), (g2,lst)) | lst g1 g2. iv g1 \<longrightarrow> iv g2}"
+
+definition mono_pop :: "('a \<Rightarrow> bool) \<Rightarrow> ('a * 'a list) rel" where
+"mono_pop iv =
+   {((g1,a#lst), (g2,lst)) | lst g1 g2 a. iv g1 \<longrightarrow> iv a \<longrightarrow> iv g2}"
+
+definition mono_push :: "('a \<Rightarrow> bool) \<Rightarrow> ('a * 'a list) rel" where
+"mono_push iv =
+   {((g1,lst), (g2,a#lst)) | lst g1 g2 a. iv g1 \<longrightarrow> iv a} \<inter>
+   {((g1,lst), (g2,a#lst)) | lst g1 g2 a. iv a \<longrightarrow> iv g2}"
+
+definition mono_rules :: "('a \<Rightarrow> bool) \<Rightarrow> ('a * 'a list) rel" where
+"mono_rules iv = mono_same iv \<union> mono_pop iv \<union> mono_push iv"
+
+lemma mono_same :
+   "monoI iv a \<Longrightarrow>
+    (a,b) \<in> mono_same iv \<Longrightarrow>
+    monoI iv b"
+unfolding monoI_def mono_same_def
+  using less_SucI less_Suc_eq_0_disj by fastforce
+
+lemma mono_push :
+   "monoI iv (v1,lst) \<Longrightarrow>
+    ((v1, lst), (v2,a#lst)) \<in> mono_push iv \<Longrightarrow>
+    monoI iv (v2,a#lst)"
+unfolding monoI_def mono_push_def
+apply auto
+  apply (metis diff_Suc_1 less_Suc_eq_0_disj nth_Cons')
+  apply (metis diff_Suc_1 less_Suc_eq_0_disj nth_Cons')
+  apply (metis diff_Suc_1 less_Suc_eq_0_disj nth_Cons')
+done
+
+lemma mono_pop :
+   "monoI iv (v1,a#lst) \<Longrightarrow>
+    ((v1,a#lst), (v2,lst)) \<in> mono_pop iv \<Longrightarrow>
+    monoI iv (v2,lst)"
+unfolding monoI_def mono_pop_def
+apply auto
+  apply (metis Suc_mono length_Cons less_SucI list.sel(3) nth_Cons' nth_tl)
+  apply (metis Suc_mono length_Cons less_SucI list.sel(3) nth_Cons' nth_tl)
+  apply (metis Suc_mono length_Cons less_SucI list.sel(3) nth_Cons' nth_tl)
+done
+
+lemma mono_works :
+   "monoI iv (v1,lst1) \<Longrightarrow>
+    ((v1,lst1), (v2,lst2)) \<in> mono_rules iv \<Longrightarrow>
+    (lst1, lst2) \<in> push_pop \<Longrightarrow>
+    monoI iv (v2,lst2)"
+apply (auto simp add: push_pop_def)
+using mono_same [of iv "(v1,lst2)" "(v2,lst2)"]
+  apply (smt Int_iff Pair_inject UnE mem_Collect_eq mono_pop mono_pop_def mono_push_def mono_rules_def)
+  apply (smt Int_iff UnE fst_conv mem_Collect_eq mono_pop mono_push mono_push_def mono_rules_def mono_same snd_conv tlR_def)
+  by (smt UnE fst_conv mem_Collect_eq mono_pop mono_pop_def mono_push mono_rules_def mono_same snd_conv tlR_def)
+
+definition first :: "('a \<Rightarrow> bool) \<Rightarrow> nat \<Rightarrow> 'a list \<Rightarrow> bool" where
+"first P k lst ==
+   k < length lst \<and> P (lst!k) \<and> (\<forall>k2 < k. \<not>P (lst!k2))"
+
+definition first_return :: "nat \<Rightarrow> 'a list list \<Rightarrow> bool" where
+"first_return k lst =
+    first (\<lambda>b. b = tl (hd lst)) k lst"
+
+definition first_smaller :: "nat \<Rightarrow> nat list \<Rightarrow> bool" where
+"first_smaller k lst = first (\<lambda>b. b < hd lst) k lst"
+
+definition first_one_smaller :: "nat \<Rightarrow> nat list \<Rightarrow> bool" where
+"first_one_smaller k lst = first (\<lambda>b. Suc b = hd lst) k lst"
+
+lemma pathR_take : "pathR r lst \<Longrightarrow> pathR r (take k lst)"
+by (simp add:path_defs path_def)
+
+lemma pathR_drop : "pathR r lst \<Longrightarrow> pathR r (drop k lst)"
+by (simp add:path_defs path_def)
+
+definition clip :: "nat \<Rightarrow> nat \<Rightarrow> 'a list \<Rightarrow> 'a list" where
+"clip k k3 lst = take (k - k3 + 1) (drop k3 lst)"
+
+lemma pathR_clip : "pathR r lst \<Longrightarrow> pathR r (clip k1 k2 lst)"
+by (simp add:pathR_drop pathR_take clip_def)
+
+lemma hd_clip :
+   "k3 < k \<Longrightarrow> k < length lst \<Longrightarrow>
+    hd (clip k k3 lst) = lst!k3"
+unfolding clip_def
+  by (metis Cons_nth_drop_Suc Nat.add_0_right One_nat_def add_Suc_right list.sel(1) order.strict_trans take_Suc_Cons)
+
+lemma last_index :
+   "length lst > 0 \<Longrightarrow> last lst = lst!(length lst-1)"
+  using last_conv_nth by auto
+
+lemma last_clip :
+   "k3 < k \<Longrightarrow> k < length lst \<Longrightarrow>
+    last (clip k k3 lst) = lst!k"
+unfolding clip_def
+by (auto simp add: last_conv_nth min.absorb2)
+
+lemma hd_take : "hd (take (Suc k3) lst) = hd lst"
+  by (metis list.sel(1) take_Nil take_Suc)
+
+lemma last_take :
+  "length lst > k3 \<Longrightarrow>
+   last (take (Suc k3) lst) = lst!k3"
+  by (simp add: take_Suc_conv_app_nth)
+
+
+lemma first_smaller1 :
+   "inc_decL lst \<Longrightarrow>
+    first_one_smaller k lst \<Longrightarrow>
+    first_smaller k lst"
+apply (cases "length lst > 0")
+apply (auto simp:first_one_smaller_def first_def first_smaller_def)
+subgoal for k3
+using list_all_values [of "take (Suc k3) lst"]
+apply (auto simp:inc_decL_def pathR_take hd_clip last_clip
+  hd_take last_take)
+apply (cases "lst!k \<in> set (take (Suc k3) lst)")
+  apply (smt Suc_leI in_set_conv_nth le_neq_implies_less length_take min.absorb2 nth_take order.strict_trans)
+  by (simp add: less_Suc_eq_le set_mp)
+done
+
+lemma inc_dec_too_large :
+"z \<ge> y \<Longrightarrow>
+ (z, x) \<in> inc_dec \<Longrightarrow>  
+ Suc x < y \<Longrightarrow> False"
+by (auto simp add:inc_dec_def sucR_def)
+
+lemma first_smaller2 :
+   "inc_decL lst \<Longrightarrow>
+    first_smaller k lst \<Longrightarrow>
+    first_one_smaller k lst"
+apply (cases "length lst > 0")
+apply (auto simp:first_one_smaller_def first_def first_smaller_def)
+using list_all_values [of "take (Suc k) lst"]
+apply (auto simp:inc_decL_def pathR_take hd_clip last_clip
+  hd_take last_take)
+apply (cases "Suc (lst ! k) < hd lst"; auto)
+apply (cases "length lst > 1"; auto)
+defer
+apply (cases "length lst = 1"; auto)
+  apply (simp add: hd_conv_nth)
+apply (rule inc_dec_too_large [of "hd lst" "lst!(k-1)" "lst!k"])
+apply auto
+  apply (metis diff_is_0_eq diff_less dual_order.strict_implies_order hd_conv_nth less_Suc_eq_le not_le)
+apply (auto simp add:path_defs path_def)
+  by (smt One_nat_def Suc_eq_plus1 Suc_lessI Suc_n_not_le_n diff_less hd_conv_nth less_diff_conv less_or_eq_imp_le neq0_conv)
 
 end
