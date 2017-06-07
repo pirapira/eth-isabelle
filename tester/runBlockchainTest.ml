74f3c13cfb4a015e0cb02917c8b6d27197d526c2
Yoichi Hirai <i@yoichihirai.com>
2017-06-07 17:29:14 +0200

    runBlockchainTest: now lists files

--- tester/runBlockchainTest.ml
+++ tester/runBlockchainTest.ml
@@ -1,2 +1,8 @@
+let test_one_file path =
+  let () = Printf.printf "file: %s\n" path in
+  ()
+
 let _ =
-  Printf.printf "blockchaintest.\n"
+  let () = Printf.printf "runBlockchainTest is not running any test yet. \n%!" in
+  let () = TraverseJsons.traverse "../tests/BlockchainTests" test_one_file in
+  ()
