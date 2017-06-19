f0d44db7b327f7d9bb4039d58d03478f9263d89e
Yoichi Hirai <i@yoichihirai.com>
2017-06-19 15:02:50 +0200

    Ignore files with unsupported encodings for now

--- tester/runBlockchainTest.ml
+++ tester/runBlockchainTest.ml
@@ -4,9 +4,13 @@ let test_one_file path =
   let () = Printf.printf "file: %s\n" path in
   let j = Yojson.Basic.from_file path in
   let () = Printf.printf ".... JSON parsed!\n" in
-  let () = ignore (BlockchainTestParser.parse_test_file j) in
-  let () = Printf.printf ".... test parsed!\n" in
-  ()
+  try
+    let () = ignore (BlockchainTestParser.parse_test_file j) in
+    let () = Printf.printf ".... test parsed!\n" in
+    ()
+  with BlockchainTestParser.UnsupportedEncoding ->
+    let () = Printf.printf ".... has an unsupported encoding.\n" in
+    ()
 
 let _ =
   let () = Printf.printf "runBlockchainTest is not running any test yet. \n%!" in
