8f9b76cb740406d66e086e52add93ff5ede24443
Yoichi Hirai <i@yoichihirai.com>
2017-06-20 15:56:55 +0200

    Started implementing the blockchain test execution

--- tester/runBlockchainTest.ml
+++ tester/runBlockchainTest.ml
@@ -1,12 +1,19 @@
 open Yojson.Basic
 
+let test_one_case (path : string) (case_name, test) =
+  let () =
+    (if List.length test.BlockchainTestParser.bcCaseBlocks <> 1 then
+       Printf.printf "...... skipping %s\n" case_name) in
+  ()
+
 let test_one_file path =
   let () = Printf.printf "file: %s\n" path in
   let j = Yojson.Basic.from_file path in
   let () = Printf.printf ".... JSON parsed!\n" in
   try
-    let () = ignore (BlockchainTestParser.parse_test_file j) in
+    let testCases = BlockchainTestParser.parse_test_file j in
     let () = Printf.printf ".... test parsed!\n" in
+    let () = List.iter (test_one_case path) testCases in
     ()
   with BlockchainTestParser.UnsupportedEncoding ->
     let () = Printf.printf ".... has an unsupported encoding.\n" in
